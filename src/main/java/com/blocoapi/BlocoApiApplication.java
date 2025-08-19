6639938d38eb6853c122c60368acc18f211af0f8
Igor Borges <borgesigor153@gmail.com>
2025-08-19 16:55:12 -0300

    :tada: init: primeiro commit

---
+++ src/main/java/com/blocoapi/BlocoApiApplication.java
@@ -0,0 +1,13 @@
+package com.blocoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlocoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blocoapi/BlocoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blocoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
