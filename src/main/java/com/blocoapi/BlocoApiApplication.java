095d0ad9f8be92c7e2b4b4dc7ab6028a0af3a36e
Igor Borges <borgesigor153@gmail.com>
2026-08-16 18:09:54 -0300

    :sparkles: feat: criando arquivo .env

--- src/main/java/com/blocoapi/BlocoApiApplication.java
+++ src/main/java/com/blocoapi/BlocoApiApplication.java
@@ -1,5 +1,6 @@
 package com.blocoapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlocoApiApplication {
 
 	public static void main(String[] args) {
+		loadDotenv();
 		SpringApplication.run(BlocoApiApplication.class, args);
+	}
+
+	private static void loadDotenv() {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			String key = entry.getKey();
+			String value = entry.getValue();
+
+			if (System.getProperty(key) == null && System.getenv(key) == null) {
+				System.setProperty(key, value);
+			}
+		});
 	}
 
 }
